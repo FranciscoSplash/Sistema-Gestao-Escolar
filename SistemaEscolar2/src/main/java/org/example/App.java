ac31715b62bfec475174849641d93b7992011928
Francisco Mauricio <franciscomauricio796@gmail.com>
2026-05-06 21:57:41 -0300

    Construcao do modelo e do dao

---
+++ SistemaEscolar2/src/main/java/org/example/App.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import database.sqlConn;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        sqlConn.testarContection();
+
+
+    }
+
+
+}
